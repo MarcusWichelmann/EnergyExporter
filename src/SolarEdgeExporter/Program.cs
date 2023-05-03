d3cffaf1b2372122b1b64df6e764282cf816242e
Marcus Wichelmann <mail@marcusw.de>
2023-05-03 23:28:39 +0200

    Update to .NET 7.0 and minimal API

--- src/SolarEdgeExporter/Program.cs
+++ src/SolarEdgeExporter/Program.cs
@@ -1,18 +1,67 @@
-using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using SolarEdgeExporter.InfluxDb;
+using SolarEdgeExporter.Options;
+using SolarEdgeExporter.Prometheus;
+using SolarEdgeExporter.Services;
+using SolarEdgeExporter.Utils;
 
-namespace SolarEdgeExporter
-{
-    public static class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+WebApplicationBuilder builder = WebApplication.CreateBuilder();
 
-        public static IHostBuilder CreateHostBuilder(string[] args)
-            => Host.CreateDefaultBuilder(args).UseSystemd().ConfigureWebHostDefaults(webBuilder => {
-                webBuilder.UseStartup<Startup>();
-            });
-    }
-}
+builder.Host.UseSystemd();
+
+// Register options
+builder.Services.AddOptions<DevicesOptions>().Bind(builder.Configuration.GetSection(DevicesOptions.Key))
+    .RecursivelyValidateDataAnnotations();
+builder.Services.AddOptions<PollingOptions>().Bind(builder.Configuration.GetSection(PollingOptions.Key))
+    .RecursivelyValidateDataAnnotations();
+builder.Services.AddOptions<ExportOptions>().Bind(builder.Configuration.GetSection(ExportOptions.Key))
+    .RecursivelyValidateDataAnnotations();
+
+var indentJson = builder.Configuration.GetValue<bool>($"{ExportOptions.Key}:{nameof(ExportOptions.IndentedJson)}");
+
+// Add API support
+builder.Services.AddControllers().AddXmlSerializerFormatters().AddJsonOptions(options => {
+    options.JsonSerializerOptions.WriteIndented = indentJson;
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
+builder.Services.AddProblemDetails();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options => {
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "SolarEdge Exporter", Version = "v1" });
+});
+
+// Register services
+builder.Services.AddSingleton<MetricsWriter>();
+builder.Services.AddSingleton<InfluxDbExporter>();
+builder.Services.AddSingleton<DeviceService>();
+builder.Services.AddHostedService<DevicePollingService>();
+
+WebApplication app = builder.Build();
+
+app.UseExceptionHandler();
+app.UseStatusCodePages();
+
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SolarEdge Exporter v1"));
+
+// Info text
+app.MapGet("/", context => context.Response.WriteAsync("SolarEdge Exporter"));
+
+// API routes
+app.MapControllers();
+
+// Metrics endpoint
+app.MapGet("/metrics", context => {
+    context.Response.ContentType = "text/plain";
+    var metricsWriter = context.RequestServices.GetRequiredService<MetricsWriter>();
+    return metricsWriter.WriteToStreamAsync(context.Response.Body);
+});
+
+app.Run();
